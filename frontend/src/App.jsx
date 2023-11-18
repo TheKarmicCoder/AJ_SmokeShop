435dd450412c7e7a23adc6414562b99be0fb3b0f
Jordan Golden <112906949+TheKarmicCoder@users.noreply.github.com>
2023-11-18 10:11:06 -0500

    i believe i fixed the issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,45 @@
-import  { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import AppRoutes from './components/AppRoutes';
 
 function App() {
+  const [showVideo, setShowVideo] = useState(false);
+
   useEffect(() => {
-    const video = document.getElementById('background-video');
+    const isMobile = window.innerWidth <= 768; // Adjust the width as needed
 
-    const handleInteraction = () => {
-      if (video) {
-        video.play().catch(error => {
-          // Handle play error if needed
-          console.error('Error playing video:', error);
-        });
-        video.removeEventListener('touchstart', handleInteraction);
-        video.removeEventListener('click', handleInteraction);
-      }
-    };
-
-    if (video) {
-      video.addEventListener('touchstart', handleInteraction);
-      video.addEventListener('click', handleInteraction);
+    // If it's a mobile device, show the landing page
+    if (isMobile) {
+      setShowVideo(false);
+    } else {
+      setShowVideo(true);
     }
-
-    return () => {
-      if (video) {
-        video.removeEventListener('touchstart', handleInteraction);
-        video.removeEventListener('click', handleInteraction);
-      }
-    };
   }, []);
+
+  const handleEnter = () => {
+    setShowVideo(true);
+  };
 
   return (
     <div className="App">
-      <video autoPlay muted loop id="background-video" >
-        <source src="https://res.cloudinary.com/diwe7m0es/video/upload/v1699135918/Thin_Smoke_65___45s___4k_res_shb3va.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video> 
-      <div className="overlay"></div>
-      <AppRoutes />
+      {showVideo ? (
+        <div className="video-container">
+          <video autoPlay muted loop id="background-video">
+            <source src="https://res.cloudinary.com/diwe7m0es/video/upload/v1699135918/Thin_Smoke_65___45s___4k_res_shb3va.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <div className="overlay"></div>
+          <AppRoutes />
+        </div>
+      ) : (
+        <div className="landing-page">
+          <h1>Welcome to the Smoke Shop</h1>
+          <button className="enter-button" onClick={handleEnter}>
+            Enter
+          </button>
+          <p>Click the button to experience the full atmosphere</p>
+        </div>
+      )}
     </div>
   );
 }
